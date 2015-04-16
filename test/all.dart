bf3c3ae3e971d5fe0aef09eb57f66f76a547c792
pquitslund <pquitslund@gmail.com>
2015-04-16 13:27:06 -0700

    Option tests; coveralls config.

---
+++ test/all.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:unittest/unittest.dart';
+
+import 'options_test.dart' as options;
+
+main() {
+  // Tidy up output.
+  filterStacks = true;
+  formatStacks = true;
+
+  options.main();
+}
--- tool/travis.sh
+++ tool/travis.sh
@@ -10,7 +10,18 @@ set -e
 # Verify that the libraries are error free.
 dartanalyzer --fatal-warnings \
   bin/analyzer.dart \
-  lib/src/analyzer_impl.dart
+  lib/src/analyzer_impl.dart \
+  test/all.dart
 
 # Run the tests.
-# dart test/all.dart
+dart test/all.dart
+
+# Install dart_coveralls; gather and send coverage data.
+if [ "$COVERALLS_TOKEN" ]; then
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report \
+    --token $COVERALLS_TOKEN \
+    --retry 2 \
+    --exclude-test-files \
+    test/all.dart
+fi
