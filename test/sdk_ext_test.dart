6c552ad4a3dcc9fa8a169f12b3f194333bd3b036
William Hesse <whesse@google.com>
2015-08-14 15:43:36 +0200

    Annotate tests to only run on command-line Dart vm.
    BUG=
    R=paulberry@google.com
    
    Review URL: https://codereview.chromium.org//1292763002 .

--- test/sdk_ext_test.dart
+++ test/sdk_ext_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@TestOn("vm")
+
 /// Test that sdk extensions are properly detected in various scenarios.
 library analyzer_cli.test.sdk_ext;
 
